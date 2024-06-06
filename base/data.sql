8c12baa6699d202ca8e7e5f9ad53c0b5bc9fede7
Mahaliana <153856982+mahalianafitiavana@users.noreply.github.com>
2024-06-06 12:40:55 -0700

    Create data.sql

---
+++ base/data.sql
@@ -0,0 +1,11 @@
+
+drop table genre;
+drop table bonus;
+drop table collecteur ;
+
+insert  into genre values
+(1, 'Femme'),
+(2, 'Homme');
+
+insert  into collecteur values
+(default, 'Mahaliana', 'XXXX','Antanimenakely','2024-04-04', 1);

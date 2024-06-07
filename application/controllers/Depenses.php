d699e6bcb9ea6b06b0e1df3ded4bdb71bdecf683
ChanAlex2357 <jacqueschanalex@gmail.com>
2024-06-07 22:20:48 +0300

    Controller de depense

---
+++ application/controllers/Depenses.php
@@ -0,0 +1,8 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Depenses extends CI_Controller {
+    public function formulaire(){
+		$this->load->view("templates/template");
+    }
+}

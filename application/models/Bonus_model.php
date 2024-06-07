1e17b453ba5c975cd4c376d7b3e4ce782da2dcec
Mahaliana <153856982+mahalianafitiavana@users.noreply.github.com>
2024-06-07 05:26:55 -0700

    Update Bonus_model.php
    save and find all

--- application/models/Bonus_model.php
+++ application/models/Bonus_model.php
@@ -2,15 +2,11 @@
 defined('BASEPATH') OR exit('No direct script access allowed'); 
 
 class Bonus_model extends CI_Model {
-
-    function save ($id, $date, $amount) {
-
-        //$query = $this->db->querry('insert into Bonus values (default,?,?,?)');
-       
-
-
+    function save ( $date, $amount) {
+        $sql = "insert into Bonus (datedebut , amount) values (?,?)"; 
+        $this->db->query($sql, array($date, $amount));
     }
-    function findAll () {
+    function find_all () {
         $query = $this->db->query('select * from Bonus');
         return $query->result_array();
     }

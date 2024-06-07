bd2ae0423e6030d9ed9fa0525baa85996abd778e
ChanAlex2357 <jacqueschanalex@gmail.com>
2024-06-07 22:26:44 +0300

    template pour l'affichage de depense

---
+++ application/views/templates/depenses.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/style.css') ?>">
+    <title>Depenses</title>
+</head>
+<body>
+    <?php
+        $this->load->view($contents);
+    ?>
+</body>
+</html>

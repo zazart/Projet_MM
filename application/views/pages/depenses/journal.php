638ec4085788db9b9d340416578b71eb9797fa82
ChanAlex2357 <jacqueschanalex@gmail.com>
2024-06-08 12:16:08 +0300

    view liste et navigation de journal

---
+++ application/views/pages/depenses/journal.php
@@ -0,0 +1,64 @@
+<!-- START JOURNAL -->
+<section class="row">
+    <!-- Titre journal -->
+    <div class="col-12 d-flex justify-content-center">
+        <h2>JOURNAL</h2>
+    </div>
+</section>
+<section class="row">
+    <div class="col-12">
+        <!-- Indication de saison -->
+        <div class="d-flex">
+            <h3 id="titreJouranl">Titre</h3>
+            <div id="titreSaison">
+                <b id="moisSaison">##</b>
+                <b id="anneeSaison">####</b>
+            </div>
+        </div>
+        <!-- #/ Indication de saison -->
+        <!-- Tableau de journal -->
+        <table class="table">
+            <thead>
+                <th>Date</th>
+                <th>Intitule (Description)</th>
+                <th>Categorie</th>
+                <th>Montant</th>
+                <th>Debit</th>
+                <th>Credit</th>
+                <th>Justificatif</th>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>-- / -- / --</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
+                    <td>Transport</td>
+                    <td>50 000Ar</td>
+                    <td>1</td>
+                    <td>2</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>-- / -- / --</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
+                    <td>Transport</td>
+                    <td>50 000Ar</td>
+                    <td>1</td>
+                    <td>2</td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+        <!-- #/ Tableau journal -->
+        <!-- Navigation buttons  -->
+        <div class="row">
+            <div class="col-6 justify-content-center-start">
+                <button class="btn px-12">Precedent</button>
+            </div>
+            <div class="col-6 justify-content-center-end">
+                <button class="btn px-12">Suivant</button>
+            </div>
+        </div>
+        <!-- #/ Navigation buttons -->
+    </div>
+</section>
+<!-- END JOURNAL -->

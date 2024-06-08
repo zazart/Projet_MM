04298c580f80bf351a133505f2bed4b1bc2b1010
Mahaliana <153856982+mahalianafitiavana@users.noreply.github.com>
2024-06-08 11:35:27 -0700

    Update Collect_model.php
    fix method find_all

--- application/models/Collect_model.php
+++ application/models/Collect_model.php
@@ -6,11 +6,11 @@ class Collect_model extends CI_Model {
         $this->db->query($sql, array($date, $qtt, $matiere, $collecteur));
     }
     function find_all () {
-        $query = 'select  c.id_collects, c.datecollect as dates,c.qtt ,c.id_collecteur, col.nom as collecteur,c.id_matierpremier as id_matiere , mp.nom as matiere from collects c';
-        $query .= ' join matierpremier mp on mp.id_matierpremier = c.id_matierpremier';
-        $query .= ' join collecteur col on c.id_collecteur = col.id_collecteur';
+        $sql = 'select  c.id_collects, c.datecollect as dates,c.qtt ,c.id_collecteur, col.nom as collecteur,c.id_matierpremier as id_matiere , mp.nom as matiere from collects c';
+        $sql .= ' join matierpremier mp on mp.id_matierpremier = c.id_matierpremier';
+        $sql .= ' join collecteur col on c.id_collecteur = col.id_collecteur';
 
-        $query = $this->db->query($query);
+        $query = $this->db->query($sql);
         return $query->result_array();
     }
     function update ($id,$date, $qtt, $matiere, $collecteur){

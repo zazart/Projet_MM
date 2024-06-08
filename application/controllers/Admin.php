29e33f140f35e2f3e0b9bcbdab8b54900c3afaf4
Mahaliana <153856982+mahalianafitiavana@users.noreply.github.com>
2024-06-08 00:13:36 -0700

    Update Admin.php

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -2,10 +2,10 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Admin extends CI_Controller {
-    public function __construct(){
-        parent::__construct();
-    }
-    public function index(){
+    public function bonus(){
         $this->load->view('bonus');
-    }   
+    } 
+    public function index(){
+        $this->load->view('collecteur');
+    }
 }

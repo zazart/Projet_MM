5eb2d7b163d13b08d158a8a5f85dc64e3ae4f7c7
Mahaliana <153856982+mahalianafitiavana@users.noreply.github.com>
2024-06-08 00:12:40 -0700

    Update CollecteurController.php
    add save with validation

--- application/controllers/CollecteurController.php
+++ application/controllers/CollecteurController.php
@@ -12,6 +12,36 @@ class CollecteurController extends CI_Controller{
             ->set_output(json_encode($collecteur));
     }
     public function save(){
+        $this->form_validation->set_rules('nom', 'nom', 'required',
+            array('required' => 'Le champ est obligatoire'            )
+        );
+        $this->form_validation->set_rules('contact', 'contact', 'required|numeric',
+            array('required' => 'Le champ est obligatoire',
+                    'numeric' => 'Veuillez ne selectionner que des chiffres'         
+        ));
+        $this->form_validation->set_rules('adresse', 'adresse', 'required',
+            array('required' => 'Le champ est obligatoire'            )
+        );
+        $this->form_validation->set_rules('date', 'date', 'required',
+            array('required' => 'Le champ est obligatoire'            )
+        );
+        $this->form_validation->set_rules('genre', 'genre', 'required',
+            array('required' => 'Le champ est obligatoire'            )
+        );
+        if ($this->form_validation->run() == FALSE) {
+            $this->load->view('collecteur');
+        } else {
+            $nom = $this->input->post('nom');
+            $contact = $this->input->post('contact');
+            $adresse = $this->input->post('adresse');
+            $date = $this->input->post('date');
+            $genre = $this->input->post('genre');
+            $this->Collecteur_model->save($nom,$contact,$adresse, $date, $genre);
+            $this->output
+                ->set_content_type('application/json')
+                ->set_output(json_encode(array('message' => 'success')));
+        }
+        
         
     }
 }
